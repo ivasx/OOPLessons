5c3f078a4665c1ae6870cd35fb97e5b0316d0c88
ivasx <ambroziak.v.ivan@gmail.com>
2025-03-17 13:01:50 +0200

    Complited Task1 Phone Object

--- Task1/Main.java
+++ Task1/Main.java
@@ -1,49 +1,26 @@
-import java.util.Scanner;
-import java.util.Random;
+public class Main{
+   public static void main(String[] args) {
+       Phone iPhone12 = new Phone();
+       iPhone12.brand = "iPhone";
+       iPhone12.model = "12";
+       iPhone12.price = 20000;
+       iPhone12.showInfo();
 
-public class Main {
-    public static void main(String[] args) {
-        NumberGuess coldAndHot = new NumberGuess();
-        coldAndHot.guessTheNumber();
-        }
-    }
 
-class NumberGuess {
-    Scanner scan = new Scanner(System.in);
-    Random random = new Random();
-    boolean equality = true;
-    int guessedNumber = random.nextInt(101);
+   }
+}
 
-    void validationNumber(int userNumber){
-        if(userNumber == 0) {
-            printNumber();
-        } else if(userNumber < 0 || userNumber > 100) {
-            System.out.println("Введене число Не в діапазоні від 1 до 100. Спробуйте ще раз: ");
-            userNumber = scan.nextInt();
-        }
-        compareNumbers(userNumber);
-    }
 
-    void compareNumbers(int userNumber) {
-        if(userNumber < guessedNumber) {
-            System.out.println("Загадане число більше. Спробуйте ще раз: ");
-        } else if (userNumber > guessedNumber){
-            System.out.println("Загадане число менше. Спробуйте ще раз: ");
-        } else {
-        equality = false;
-        System.out.println("Ви вгадали число, загадане число: " + guessedNumber);
-        }
-    }
+class Phone {
+   String brand;
+   String model;
+   int price;
 
-    public void guessTheNumber() {
-        System.out.print("Введіть число від 1 до 100: ");
-        while (equality) {
-            int userNumber = scan.nextInt();
-            validationNumber(userNumber);
-        }
-    }
 
-    void printNumber(){
-        System.out.println("Загадане число: " + guessedNumber);
-    }
+   void showInfo(){
+       System.out.println("Інформація про телефон: ");
+       System.out.println("Бренд: " + brand);
+       System.out.println("Модель: " + model);
+       System.out.println("Ціна: " + price + " грн.");
+   }
 }
