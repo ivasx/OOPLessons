20b8f4725a4393e70273cec537246446b72c9ea2
ivasx <ambroziak.v.ivan@gmail.com>
2025-04-10 13:24:21 +0300

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,7 @@
+public class Cat extends Animal{
+    public Cat(String name, kindAnimal kind, int age, double weight, int hungerLevel, String location) {
+        super(name, kind, age, weight, hungerLevel, location);
+    }
+
+    
+}
