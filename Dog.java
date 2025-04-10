b37ff086f4936cf00a8999bb3aeca233f3d4b3e8
ivasx <ambroziak.v.ivan@gmail.com>
2025-04-10 13:24:04 +0300

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,7 @@
+public class Dog extends Animal{
+    public Dog(String name, kindAnimal kind, int age, double weight, int hungerLevel, String location) {
+        super(name, kind, age, weight, hungerLevel, location);
+    }
+
+    
+}
