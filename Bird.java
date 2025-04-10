2602e7ff8f0463a513d981d6f4df4aa042088823
ivasx <ambroziak.v.ivan@gmail.com>
2025-04-10 13:24:36 +0300

    Create Bird.java

---
+++ Bird.java
@@ -0,0 +1,7 @@
+public class Bird extends Animal{
+    public Bird(String name, kindAnimal kind, int age, double weight, int hungerLevel, String location) {
+        super(name, kind, age, weight, hungerLevel, location);
+    }
+
+    
+}
